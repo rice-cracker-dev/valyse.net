658f6fc807f2e2a712fb351720d9dc575f817baa
rice-cracker-dev <ricecracker2234@outlook.com>
2023-08-08 19:05:34 +0700

    added RICKROLL HAHAHAHHA

---
+++ src/lib/components/Troll.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let isVisible = false;
+
+  onMount(() => {
+    const isCaptchaClosed = localStorage.getItem('isCaptchaClosed');
+
+    isVisible = isCaptchaClosed === null;
+  });
+</script>
+
+{#if isVisible}
+  <div class="fixed z-[99999] w-[100svw] h-[100svh] flex justify-center items-center bg-black/75">
+    <div class="flex flex-col items-center gap-4">
+      <p class="font-semibold text-xl">Please verify that you're human</p>
+      <a
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        target="_blank"
+        on:click={() => {
+          localStorage.setItem('isCaptchaClosed', 'true');
+          isVisible = false;
+        }}
+      >
+        <img src="/captcha.png" alt="" class="w-[20rem]" />
+      </a>
+    </div>
+  </div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import Navbar from '$lib/components/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import Troll from '$lib/components/Troll.svelte';
 
   import '@fontsource-variable/inter';
   import '@fontsource-variable/outfit';
   import '../app.css';
 </script>
+
+<Troll />
 
 <Navbar />
 
