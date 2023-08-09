458646d1f7a20b6fbb6720476b9e9e5ea63b3960
rice-cracker-dev <ricecracker2234@outlook.com>
2023-08-09 21:49:50 +0700

    added cool effect

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -4,13 +4,34 @@
   export let icon: string;
   export let header: string;
   export let isSpecial = false;
+
+  let isMouseHover = false;
+  let element: HTMLDivElement;
+  let glowOffsetX = 0;
+  let glowOffsetY = 0;
 </script>
 
 <div
-  class="border-[1px] bg-neutral/50 p-4 rounded-lg flex flex-col gap-4 shadow-2xl {isSpecial
+  on:pointerenter={() => (isMouseHover = true)}
+  on:pointerleave={() => (isMouseHover = false)}
+  on:pointermove={(o) => {
+    if (isMouseHover) {
+      const bounding = element.getBoundingClientRect();
+      glowOffsetX = o.clientX - bounding.left;
+      glowOffsetY = o.clientY - bounding.top;
+    }
+  }}
+  bind:this={element}
+  class="overflow-hidden relative border-[1px] bg-neutral/50 p-4 rounded-lg flex flex-col gap-4 shadow-2xl {isSpecial
     ? 'border-accent shadow-accent/10 bg-neutral'
     : 'border-base-content/20 shadow-transparent hover:bg-neutral hover:border-accent hover:shadow-accent/10'}"
 >
+  <div
+    class="absolute overflow-hidden rounded-[50%] blur-3xl duration-0"
+    style="left: {glowOffsetX}px; top: {glowOffsetY}px"
+  >
+    <div class="w-16 h-16 duration-500 {isMouseHover ? 'bg-white/50 scale-100' : 'bg-transparent scale-0'}" />
+  </div>
   <div class="flex items-center gap-4">
     <div class="p-2 border-[1px] border-base-content/20 bg-base-100 rounded-lg">
       <Icon {icon} class="text-2xl" />
